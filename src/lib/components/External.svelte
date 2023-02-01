e1befa72e12c0e185dbddfa18855da329c8853af
Kyle Niemeyer <kyleniemeyer@fastmail.com>
2023-02-01 17:51:47 -0500

    adds mastadon rel attribute to verify

--- src/lib/components/External.svelte
+++ src/lib/components/External.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     export let to: string;
+    export let ref: string = "noreferrer";
 </script>
 
-<a href={to} target="_blank" rel="noreferrer"><slot></slot></a>
+<a href={to} target="_blank" rel={ref}><slot></slot></a>
